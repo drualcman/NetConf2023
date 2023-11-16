c69d06c76dddf1e54934738ab813cce48aba454b
Sergi Ortiz Gómez <drualcman@msn.com>
2023-11-16 17:32:21 +0800

    Add project files.

---
+++ NetConf2023.Pagination/Handlers/PaginationObjectHangler.cs
@@ -0,0 +1,13 @@
+﻿namespace NetConf2023.Pagination.Handlers;
+public class PaginationObjectHangler<TData> : PaginatorHangler
+{
+    readonly IReadOnlyList<TData> Data;
+
+    public PaginationObjectHangler(IReadOnlyList<TData> data, int pageSize) : base(data.Count, pageSize) =>
+        Data = data;
+
+    public bool HasItems => Data.Count > 0;
+    public IEnumerable<TData> Items => Data.Skip(ToSkip).Take(PageSize);
+
+    int ToSkip => (ActualPage - 1) * PageSize;
+}
